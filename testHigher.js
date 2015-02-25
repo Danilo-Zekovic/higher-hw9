7a4fdeb3db317083e1a999c8d20e4026082aa4f3
Danilo Zekovic <dzv9776@saintjoe.edu>
2015-02-24 23:20:31 -0500

    file to create set and populate it

---
+++ testHigher.js
@@ -0,0 +1,16 @@
+load("set.js");
+// create set
+var numbers = new Set();
+
+// add elements to numbers set
+numbers.add(2);
+numbers.add(5);
+numbers.add(3);
+numbers.add(9);
+numbers.add(6);
+numbers.add(7);
+numbers.add(1);
+numbers.add(4);
+numbers.add(8);
+
+print(numbers.show());
